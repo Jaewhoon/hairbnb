9e10a2bf2fee8681a378b0f2d18ac92f923ab2d9
Jaewhoon Cho <aerospace105@gmail.com>
2022-07-26 01:04:31 +0900

    room detail configuration with function based view

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as models_core
 from users import models as models_users
@@ -95,6 +96,9 @@ class Room(models_core.TimeStampedModel):
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def total_rating(self):
         all_reviews = self.reviews.all()
